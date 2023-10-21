5aeecb4f734cb982ff24efbf4983f5fa38b2300d
akielaries <56521583+akielaries@users.noreply.github.com>
2023-10-21 04:28:54 +0000

    Deploying to gh-pages from @ NAU-IoT/benchmarks@890249d4f067323a9cabdf58cd5287d0329b5daa 🚀

---
+++ search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "bi",
+  1: "i",
+  2: "b"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files",
+  2: "Pages"
+};
+
