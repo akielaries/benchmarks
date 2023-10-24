add594f6db399f23228f0280194b1141d018d0dd
Akiel Aries <56521583+akielaries@users.noreply.github.com>
2023-10-24 02:49:47 -0700

    Create daemon.cpp. stub this out eventually

---
+++ daemon.cpp
@@ -0,0 +1,9 @@
+/**
+  * This file serves as a background process/daemon collecting system 
+  * performance information. This will eventually be used for grafana
+  */
+
+int main () {
+
+    return 0;
+}
