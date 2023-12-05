6b1678e119e9e3cd6eef2b67827ceaf7292b0957
akielaries <akiel@akiel.org>
2023-12-05 14:09:37 -0700

    adding flags

--- benchmark.cpp
+++ benchmark.cpp
@@ -6,20 +6,19 @@
  * -b (BENCHMARK)   run benchmark system stress programs
  */
 
-#include "lib/montecarlo.hpp"   // monte carlo methods
-#include "lib/primes.hpp"       // prime number methods
-#include "lib/sys.hpp"          // system information methods
-#include "lib/threadpool.hpp"   // threadpool methods
-#include <chrono>               // cpp timing related methods
-#include <cstring>              // C styled strings
-#include <ctime>                // C time related methods
-#include <fstream>              // for file RW
-#include <iostream>             // for std IO
-#include <thread>               // for thread access
-#include <vector>               // vector DS
-#include <unistd.h>             // POSIX API
-#include <limits.h>             // defined limit constants
-
+#include "lib/montecarlo.hpp" // monte carlo methods
+#include "lib/primes.hpp"     // prime number methods
+#include "lib/sys.hpp"        // system information methods
+#include "lib/threadpool.hpp" // threadpool methods
+#include <chrono>             // cpp timing related methods
+#include <cstring>            // C styled strings
+#include <ctime>              // C time related methods
+#include <fstream>            // for file RW
+#include <iostream>           // for std IO
+#include <limits.h>           // defined limit constants
+#include <thread>             // for thread access
+#include <unistd.h>           // POSIX API
+#include <vector>             // vector DS
 
 void bench_naive_primes(std::vector<uint32_t> nums) {
     // TODO at the end of each benchmark run we should log memory
@@ -152,8 +151,8 @@ void gpu_bench_monte_carlo() {
 void daemon() {
     // System class obj
     System sys;
-    //float starting_cpu_temp = sys.cpu_idle_temp();
-    //float starting_cpu_usg = sys.cpu_stats();
+    // float starting_cpu_temp = sys.cpu_idle_temp();
+    // float starting_cpu_usg = sys.cpu_stats();
     // std::cout << starting_cpu_temp << starting_cpu_usg << std::endl;
 
     // infinite loop for continuous collection
@@ -192,14 +191,13 @@ void daemon() {
             std::cout << curr_cpu_temp << "\n" << curr_cpu_usg << std::endl;
 
             // virtual memory
-            std::cout << "TOTAL VMEM: " << sys.v_mem_total 
-                    << "\n FREE VMEM: " << sys.v_mem_free 
-                    << "\n USED VMEM: " << sys.v_mem_used << std::endl;
+            std::cout << "TOTAL VMEM: " << sys.v_mem_total
+                      << "\n FREE VMEM: " << sys.v_mem_free
+                      << "\n USED VMEM: " << sys.v_mem_used << std::endl;
             // physical memory
-            std::cout << "TOTAL PMEM: " << sys.p_mem_total 
-                    << "\n FREE PMEM: " << sys.p_mem_free 
-                    << "\n USED PMEM: " << sys.p_mem_used << std::endl;
-
+            std::cout << "TOTAL PMEM: " << sys.p_mem_total
+                      << "\n FREE PMEM: " << sys.p_mem_free
+                      << "\n USED PMEM: " << sys.p_mem_used << std::endl;
 
             // WRITE ALL INFO TO CSV FILE
             csvFile << curr_cpu_temp << ","; // Add other data as needed
@@ -219,10 +217,12 @@ void daemon() {
 }
 
 void usage(const char *programName) {
-    std::cout << "Usage: " << programName << " [-d | -b]" << std::endl;
+    std::cout << "Usage: " << programName << " [-d | -b cpu | gpu ]" << std::endl;
     std::cout << "  -d : daemon mode to monitor system information\n";
     std::cout << "  -b : benchmark mode to run system stress tests with live "
                  "monitoring\n";
+    std::cout << "    cpu - run CPU-based benchmarks";
+    std::cout << "    gpu - run GPU-based benchmarks";
 }
 
 int main(int argc, char *argv[]) {
@@ -232,88 +232,109 @@ int main(int argc, char *argv[]) {
     } else {
         if (strcmp(argv[1], "-b") == 0) {
             std::cout << "Starting benchmark...\n\n";
+            if (argc < 3) {
+                std::cerr
+                    << "Error: Specify 'cpu' or 'gpu' after the -b flag.\n";
+                exit(EXIT_FAILURE);
+            }
+
+            std::string mode(argv[2]);
 
             System sys;
 
-            // display CPU information and number of running processes from `ps`
-            sys.cpu_info();
-            sys.proc_info();
+            // if -b cpu
+            if (mode == "cpu") {
+                // display CPU information and number of running processes from
+                // `ps`
+                sys.cpu_info();
+                sys.proc_info();
 
-            sys.cpu_usage();
-            sys.mem_info();
-            sys.cpu_temp();
-            // TODO get idle temperature at the start of this benchmark to
-            // determine the "idle starting" temperature. store in struct? when
-            // would it get called?
-            // TODO FIXME I am thinking this system info should be logged from a
-            // separate thread
+                sys.cpu_usage();
+                sys.mem_info();
+                sys.cpu_temp();
+                // TODO get idle temperature at the start of this benchmark to
+                // determine the "idle starting" temperature. store in struct?
+                // when would it get called?
+                // TODO FIXME I am thinking this system info should be logged
+                // from a separate thread
 
-            std::cout
-                << "Starting with primality testing using the Miller-Rabin "
-                   "algorithm...\n";
+                std::cout
+                    << "Starting with primality testing using the Miller-Rabin "
+                       "algorithm...\n";
 
-            std::vector<uint32_t> nums = {
-                1000000007, // A large 32-bit integer PRIME
-                2147483647, // The largest 32-bit signed integer PRIME
-                97,         // A PRIME number
-                123456789,  // Another large 32-bit integer
-                19,         // A PRIME number
-                42,         // Just a random number
-                31,         // A PRIME number
-                987654321,  // Yet another large 32-bit integer
-                37,         // A PRIME number
-                123,        // Just another number
-                17,         // A PRIME number
-                999999999,  // And another large 32-bit integer
-                23,         // A PRIME number
-                777777777,  // Large 32-bit integer
-                13,         // A PRIME number
-                234567890,  // Large 32-bit integer
-                11,         // A PRIME number
-                987654321,  // Repeating value for demonstration
-                7,          // A PRIME number
-                8675309,    // Another large 32-bit integer
-                709,        // A PRIME number
-                5381,       // A PRIME number
-                52711,      // A PRIME number
-                167449,     // A PRIME number
-                648391,     // A PRIME number
-                1128889,    // A PRIME number
-                2269733,    // A PRIME number
-                3042161,    // A PRIME number
-                4535189,    // A PRIME number
-                7474967,    // A PRIME number
-                9737333,    // A PRIME number
-                14161729,   // A PRIME number
-                17624813,   // A PRIME number
-                19734581,   // A PRIME number
-                23391799,   // A PRIME number
-                29499439,   // A PRIME number
-                37139213    // A PRIME number
-            };
+                std::vector<uint32_t> nums = {
+                    1000000007, // A large 32-bit integer PRIME
+                    2147483647, // The largest 32-bit signed integer PRIME
+                    97,         // A PRIME number
+                    123456789,  // Another large 32-bit integer
+                    19,         // A PRIME number
+                    42,         // Just a random number
+                    31,         // A PRIME number
+                    987654321,  // Yet another large 32-bit integer
+                    37,         // A PRIME number
+                    123,        // Just another number
+                    17,         // A PRIME number
+                    999999999,  // And another large 32-bit integer
+                    23,         // A PRIME number
+                    777777777,  // Large 32-bit integer
+                    13,         // A PRIME number
+                    234567890,  // Large 32-bit integer
+                    11,         // A PRIME number
+                    987654321,  // Repeating value for demonstration
+                    7,          // A PRIME number
+                    8675309,    // Another large 32-bit integer
+                    709,        // A PRIME number
+                    5381,       // A PRIME number
+                    52711,      // A PRIME number
+                    167449,     // A PRIME number
+                    648391,     // A PRIME number
+                    1128889,    // A PRIME number
+                    2269733,    // A PRIME number
+                    3042161,    // A PRIME number
+                    4535189,    // A PRIME number
+                    7474967,    // A PRIME number
+                    9737333,    // A PRIME number
+                    14161729,   // A PRIME number
+                    17624813,   // A PRIME number
+                    19734581,   // A PRIME number
+                    23391799,   // A PRIME number
+                    29499439,   // A PRIME number
+                    37139213    // A PRIME number
+                };
 
-            bench_naive_primes(nums);
-            bench_threadpool_primes(nums);
+                bench_naive_primes(nums);
+                bench_threadpool_primes(nums);
 
-            // TODO where/when should these be called? should these functions
-            // update values located within a struct? think about this...
-            sys.cpu_usage();
-            sys.mem_info();
-            sys.cpu_temp();
+                // TODO where/when should these be called? should these
+                // functions update values located within a struct? think about
+                // this...
+                sys.cpu_usage();
+                sys.mem_info();
+                sys.cpu_temp();
 
-            bench_threadpool_primes_dispatch(nums);
+                bench_threadpool_primes_dispatch(nums);
 
-            sys.cpu_usage();
-            sys.mem_info();
-            sys.cpu_temp();
+                sys.cpu_usage();
+                sys.mem_info();
+                sys.cpu_temp();
+            }
 
+            // if -b gpu
+            else if (mode == "gpu") {
 // if host has NVCC installed
 #ifdef __NVCC__
-            std::cout << "NVIDIA DEVICE!\n";
+                std::cout << "NVIDIA DEVICE!\n";
 #endif
 
-            // monte_carlo();
+                // monte_carlo();
+            }
+            else {
+                std::cerr << "Error: Invalid mode. Specify 'cpu' or 'gpu' after -b flag\n";
+                exit(EXIT_FAILURE);
+            }
         }
+
+        // daemon
         if (strcmp(argv[1], "-d") == 0) {
             // TODO implement "daemon"
             std::cout << "Running as daemon...\n";
